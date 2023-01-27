188a8e4b943551cf25a1d78ff435ba71d7eb0dd1
Warley <warleyvods@gmail.com>
2023-01-27 17:48:18 -0300

    Adicionado tela de login

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <h1> Hello World </h1>
-  <h1> Hello World </h1>
-  <h1> Hello World </h1><h1> Hello World </h1>
-  <h1> Hello World </h1>
-  <h1> Hello World </h1>
-  <h1> Hello World </h1>
-
-
-  );
+    return (
+        <>
+            <h1> Bem vindo a uma tela de login </h1>
+            <input name="nome" placeholder="digite um nome" />
+            <input name="email" placeholder="digite um email" />
+            <button>Entrar</button>
+        </>
+    );
 }
 
 export default App;
